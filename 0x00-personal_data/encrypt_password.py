490e329ba7144198b6c41f1952840168eeb18a59
sam-fsd <kabirusam037@gmail.com>
2024-02-08 21:21:56 +0300

    Encrypting passwords

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""defines a function that hashes a password"""
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """returns a salted, hashed password, which is a byte string"""
+    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
