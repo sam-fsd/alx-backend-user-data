c46e285f18568243b19526f28f4e361b809970bb
sam-fsd <kabirusam037@gmail.com>
2024-02-13 23:31:19 +0300

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Basic auth module"""
+from api.v1.auth.auth import Auth
+from typing import List, TypeVar
+
+
+class BasicAuth(Auth):
+    """
+    BasicAuth class represents the basic authentication mechanism.
+    """
+    pass
