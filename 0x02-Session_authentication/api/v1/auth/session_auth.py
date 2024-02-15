898fff20ce002cef3f468f8c36f99632ac07b90a
sam-fsd <kabirusam037@gmail.com>
2024-02-15 13:59:35 +0300

    Create a session~

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
@@ -11,4 +12,21 @@ class SessionAuth(Auth):
     This class represents the session-based authentication mechanism.
     It is responsible for managing user sessions and verifying session tokens.
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a session for the given user ID.
+
+        Args:
+            user_id (str): The ID of the user.
+
+        Returns:
+            str: The session ID generated for the user.
+
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
