4429f5ed7b617b5a66030e030db43f994768bc5a
sam-fsd <kabirusam037@gmail.com>
2024-02-15 14:36:24 +0300

    Before request

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,13 +37,15 @@ def bef_req():
             '/api/v1/status/',
             '/api/v1/unauthorized/',
             '/api/v1/forbidden/'
+            '/api/v1/auth_session/login/'
         ]
         if auth.require_auth(request.path, excluded):
-            if auth.authorization_header(request) is None:
+            if (auth.authorization_header(request) is None
+                    and auth.session_cookie(request) is None):
                 abort(401, description="Unauthorized")
             if auth.current_user(request) is None:
                 abort(403, description="Forbidden")
-    request.current_user = auth.current_user(request)
+        request.current_user = auth.current_user(request)
 
 
 @app.errorhandler(404)
