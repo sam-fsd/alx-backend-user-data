2729557f8146b64336e6d28cd642d585e4d38c9f
sam-fsd <kabirusam037@gmail.com>
2024-02-19 16:47:35 +0300

    register user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Flask app"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
 
-
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -14,6 +15,17 @@ def message():
     :return: JSON response with a welcome message.
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=['POST'])
+def users():
+    email = request.form['email']
+    password = request.form['password']
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
